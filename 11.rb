24eeddb1a4e0e89b185078b32537365f08d45377
oselimi <selimiosman94@gmail.com>
2020-02-26 12:35:52 +0100

    Class_attr

--- 11.rb
+++ 11.rb
@@ -1,10 +1,14 @@
 class MyCar
-    
+    attr_accessor :color
+    attr_reader :year
     def  initialize(year,color,model)
         @year = year
         @color = color
         @model = model 
         @current_speed = 0
+    end
+    def spray_paint(color)
+        @color = color
     end
     def my_speed(speed)
         @current_speed += speed
